b2e2bfe2730c6371e664f025eeac860669b81c8d
Yuvraj Kadale <kadaleyuvraj@gmail.com>
2021-05-27 22:31:44 +0530

    Add name to main page as type

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,5 +109,21 @@
     return false;
   });
 
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
   
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
 })(jQuery);
