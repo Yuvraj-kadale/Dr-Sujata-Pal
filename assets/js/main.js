1bc962b87a5d9953a45dea81594aaf31689bb5cd
Yuvraj Kadale <kadaleyuvraj@gmail.com>
2021-06-01 23:16:17 +0530

    Add Portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,29 @@
     delay: 10,
     time: 1000
   });
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
   
   // Init AOS
   function aos_init() {
